11ed67c40034e4de778a58e1f3133d4b695dcd70
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2021-06-13 13:33:14 +0800

    Add home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+/* IMAGES */
+import Bus from '../Images/pics02.jpg';
+
 const Home = () => {
     return(
         <div className="home-main">
-            Home
+            <div className="home-container">
+                <div className="home-content">
+                    <h1>Bus Ticket Reservation made easier</h1>
+                    <h2>WE AIM TO PROVIDE MORE CONVENIENT TICKET BOOKING.</h2><br/>
+                    <img src={Bus}/><br/>
+                    <p>In compliance to minimum health protocols, temperature checks are being implemented to all passengers. Those with<br/>temperatures above 37.6 will not allowed to board the bus and will be advised to seek medical help. Wearing of face<br/>masks and face shields at all times is required.</p><br/>
+                    <button>BOOK TICKET</button>
+                </div>
+            </div>
         </div>
     )
 }
