61c2a0c74525b2862c41ce2ba5f8446d8b51c800
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2021-06-14 16:17:32 +0800

    Get value from register form

--- client/src/Components/Book.js
+++ client/src/Components/Book.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Book = () => {
+
+    const [state, setState] = useState({
+        lastName: '',
+        firstName: '',
+        middleName: '',
+        gender: '',
+        age: '',
+        address: '',
+        contactNumber: '',
+        email: '',
+        q1: '',
+        q2: '',
+        q3: ''
+    });
+
+    const handleOnChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    console.log('State: ', state);
+
     return(
         <div className="book-main">
             <div className="book-container">
@@ -9,40 +33,43 @@ const Book = () => {
                     <h2>PLEASE FILL OUT THE FORM COMPLETELY.</h2>
                 </div>
                 <form className="register-form">
-                    <input type="text" name="lastName" placeholder="Last Name"/>
-                    <input type="text" name="firstName" placeholder="First Name"/>
-                    <input type="text" name="middleName" placeholder="Middle Name"/>
+                    <input type="text" name="lastName" placeholder="Last Name" value={state.lastName} onChange={handleOnChange}/>
+                    <input type="text" name="firstName" placeholder="First Name" value={state.firstName} onChange={handleOnChange}/>
+                    <input type="text" name="middleName" placeholder="Middle Name" value={state.middleName} onChange={handleOnChange}/>
                     <div className="gender">
                         <p>Gender:</p>
-                        <input type="radio" value="Male" name="gender" />Male
-                        <input type="radio" value="Female" name="gender" />Female
+                        <input type="radio" value="Male" name="gender" onChange={handleOnChange}/>Male
+                        <input type="radio" value="Female" name="gender" onChange={handleOnChange}/>Female
                     </div>
-                    <input type="text" name="age" placeholder="Age"/>
-                    <input type="text" name="address" placeholder="Address"/>
-                    <input type="text" name="contactNumber" placeholder="Contact Number"/>
-                    <input type="text" name="email" placeholder="Email Address"/>
+                    <input type="text" name="age" placeholder="Age" value={state.age} onChange={handleOnChange}/>
+                    <input type="text" name="address" placeholder="Address" value={state.address} onChange={handleOnChange}/>
+                    <input type="text" name="contactNumber" placeholder="Contact Number" value={state.contactNumber} onChange={handleOnChange}/>
+                    <input type="text" name="email" placeholder="Email Address" value={state.email} onChange={handleOnChange}/>
                     <div className="covid-form">
                         <div className="question">
                             <p>Have you traveled outside the country anytime from December 2019 'til now?</p>
                             <div className="q1">
-                                <input type="radio" value="Yes" name="q1" />Yes
-                                <input type="radio" value="No" name="q1" />No
+                                <input type="radio" value="Yes" name="q1" onChange={handleOnChange}/>Yes
+                                <input type="radio" value="No" name="q1" onChange={handleOnChange}/>No
                             </div>
                         </div>
                         <div className="question">
                             <p>Have you come into contact with anyone who has Covid-19?</p>
                             <div className="q1">
-                                <input type="radio" value="Yes" name="q2" />Yes
-                                <input type="radio" value="No" name="q2" />No
+                                <input type="radio" value="Yes" name="q2" onChange={handleOnChange}/>Yes
+                                <input type="radio" value="No" name="q2" onChange={handleOnChange}/>No
                             </div>
                         </div>
                         <div className="question">
                             <p>Are you currently experiencing any of the following conditions during this time? (Fever, Cough, Difficulty in breathing, Headache):</p>
                             <div className="q1">
-                                <input type="radio" value="Yes" name="q3" />Yes
-                                <input type="radio" value="No" name="q3" />No
+                                <input type="radio" value="Yes" name="q3" onChange={handleOnChange}/>Yes
+                                <input type="radio" value="No" name="q3" onChange={handleOnChange}/>No
                             </div>
                         </div>
+                    </div>
+                    <div className="button">
+                        <button>Next</button>
                     </div>
                 </form>
             </div>
